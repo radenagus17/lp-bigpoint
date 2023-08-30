77ddc07c28afef4f3fa099e492e76170d168d945
radenagus17_ <vaghuz@gmail.com>
2023-08-30 08:42:09 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="border">
+        <h1 className="text-red-400 text-4xl font-bold">hallo world</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
